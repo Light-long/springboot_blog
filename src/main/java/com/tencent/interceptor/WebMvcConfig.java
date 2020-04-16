455912f4907f6f51dbad36eb9858d6420c905071
Shilong Li <lishilong1108@foxmail.com>
2020-04-16 18:17:31 +0800

    设置登录拦截器,并设置不拦截静态资源

---
+++ src/main/java/com/tencent/interceptor/WebMvcConfig.java
@@ -0,0 +1,21 @@
+package com.tencent.interceptor;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginInterceptor())
+                .addPathPatterns("/admin/**")
+                .excludePathPatterns("/admin")
+                .excludePathPatterns("/admin/login")
+                //设置不拦截静态资源
+                .excludePathPatterns("static/**");
+    }
+}
